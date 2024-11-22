a48fe6f43e33238f5da6f6c10dd09654806e7829
KorneliaOdrobinska <kornelia.odrobinska@gmail.com>
2024-11-22 11:11:52 +0100

    Assertion for LoginPage

--- src/main/java/pages/MenuItemPage.java
+++ src/main/java/pages/MenuItemPage.java
@@ -7,11 +7,15 @@ public class MenuItemPage {
 
     // TODO uproscic xpatha
     private static final String XPATH_MENU_MY_ACCOUNT = "//li[@class = 'my-account menu-item menu-item-type-post_type menu-item-object-page menu-item-201']";
+    private static final String XPATH_ENTRY_TITLE = "//h1[@class = 'entry-title']";
 
     public void myAccount(WebDriver driver){
         driver.findElement(By.xpath(XPATH_MENU_MY_ACCOUNT)).click();
     }
 
-    // TODO sprawdzic czy otworzyla sie strona MyAccount
+    public String checkPageTitle(WebDriver driver){
+        // TODO czy nie trzeba dodac łapania wyjątku gdyby elementu nie było?
+        return driver.findElement(By.xpath(XPATH_ENTRY_TITLE)).getText();
+    }
 
 }
--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,13 +1,16 @@
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.MenuItemPage;
 
 public class LoginTest extends BaseTestCase{
+
+    private static final String MY_ACCOUNT_PAGE_TITLE = "Moje konto";
     MenuItemPage menuItem;
     @Test
     private void openMyAccountPage(){
         menuItem = new MenuItemPage(); // TODO gdzie tworzyc obiekt? jeden globalny czy w kazdej metodzie?
         menuItem.myAccount(driver);
 
-        // TODO dodac assercje czy strona sie otworzyla
+        Assert.assertEquals(menuItem.checkPageTitle(driver), MY_ACCOUNT_PAGE_TITLE);
     }
 }
